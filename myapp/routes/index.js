99cac89ed69f24013ac63541385b4d337cb63fd7
Martin <95074123+CarstensMartin@users.noreply.github.com>
2022-08-06 14:41:17 +0200

    Upload study progress Level 4

---
+++ myapp/routes/index.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Maintenance Management, use localhost:3000 for Frontend' });
+});
+
+module.exports = router;
